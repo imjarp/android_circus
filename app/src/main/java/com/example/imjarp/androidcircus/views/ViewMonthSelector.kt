391ae06c409066a68ca3b8157b0a7a2ec026586c
Jorge Adrian Ramirez Perez <imjarp@localhost.localdomain>
2018-09-06 21:13:45 -0500

    wip coordinates to animate

--- app/src/main/java/com/example/imjarp/androidcircus/views/ViewMonthSelector.kt
+++ app/src/main/java/com/example/imjarp/androidcircus/views/ViewMonthSelector.kt
@@ -3,7 +3,6 @@ package com.example.imjarp.androidcircus.views
 import android.annotation.SuppressLint
 import android.content.Context
 import android.support.constraint.ConstraintLayout
-import android.support.constraint.ConstraintSet
 import android.util.AttributeSet
 import android.view.LayoutInflater
 import android.view.View
@@ -25,9 +24,10 @@ class ViewMonthSelector : ConstraintLayout, View.OnClickListener {
     private var monthTwelve: View? = null
     private var listener: MonthSelectionListener? = null
     private val interpolator = AccelerateDecelerateInterpolator()
-    private val animationDuration = resources.getInteger(android.R.integer.config_shortAnimTime).toLong()
-    private var set = ConstraintSet()
+    private val animationDuration = resources.getInteger(android.R.integer
+            .config_shortAnimTime).toLong()
     private val tag = "circus_touch"
+    private var previousMonth = MonthEnum.THREE
 
     constructor(context: Context) : super(context) {
         init(null, 0, context)
@@ -73,31 +73,45 @@ class ViewMonthSelector : ConstraintLayout, View.OnClickListener {
         v?.isSelected = true
         val monthSelected = getSelectedMonthEnum(v)
         moveFrontView(monthSelected)
-
-
         listener?.onMonthSelected(monthSelected)
     }
 
     private fun moveFrontView(monthSelected: MonthEnum) {
 
-
-        val translateAnimation = TranslateAnimation(100F, 540F, 0F, 0F)
+        val (fromX, toX) = getXCoordinates(previousMonth, monthSelected)
+        val translateAnimation = TranslateAnimation(fromX, toX, 0F, 0F)
         frontView?.animation = translateAnimation
         translateAnimation.interpolator = interpolator
         translateAnimation.duration = animationDuration
         translateAnimation.fillAfter = true
         translateAnimation.start()
-
+        previousMonth = monthSelected
     }
 
-    private fun getTranslationPoint(monthSelected: MonthEnum): Int {
-        return when (monthSelected) {
-            MonthEnum.THREE -> 0
-            MonthEnum.SIX -> 270
-            MonthEnum.NINE -> 540
-            MonthEnum.TWELVE -> 810
+    private fun getXCoordinates(previousMonth: MonthEnum, monthSelected: MonthEnum): Coordinates {
+
+        return if (isMovingToStart(previousMonth, monthSelected)) {
+            Coordinates(
+                    fromX = getViewFromMonth(monthSelected)?.left?.toFloat() ?: 0F,
+                    toX = getViewFromMonth(previousMonth)?.left?.toFloat() ?: 0F)
+        } else {
+            Coordinates(
+                    fromX = getViewFromMonth(previousMonth)?.left?.toFloat() ?: 0F,
+                    toX = getViewFromMonth(monthSelected)?.left?.toFloat() ?: 0F)
         }
     }
+
+    private fun isMovingToStart(previousMonth: MonthEnum, monthSelected: MonthEnum): Boolean =
+            monthSelected.numMonth > previousMonth.numMonth
+
+    private fun getViewFromMonth(monthEnum: MonthEnum) =
+            when (monthEnum) {
+                MonthEnum.THREE -> monthThree
+                MonthEnum.SIX -> monthSix
+                MonthEnum.NINE -> monthNine
+                MonthEnum.TWELVE -> monthTwelve
+            }
+
 
     private fun getSelectedMonthEnum(v: View?): MonthEnum {
         return when (v?.id) {
@@ -118,7 +132,6 @@ class ViewMonthSelector : ConstraintLayout, View.OnClickListener {
         }
     }
 
-
     private inline fun <T : View> T.afterMeasured(crossinline f: T.() -> Unit) {
         viewTreeObserver.addOnGlobalLayoutListener(object : ViewTreeObserver.OnGlobalLayoutListener {
             override fun onGlobalLayout() {
@@ -134,11 +147,14 @@ class ViewMonthSelector : ConstraintLayout, View.OnClickListener {
         fun onMonthSelected(month: MonthEnum)
     }
 
-    enum class MonthEnum {
-        THREE,
-        SIX,
-        NINE,
-        TWELVE
+    enum class MonthEnum(val numMonth: Int) {
+        THREE(3),
+        SIX(6),
+        NINE(9),
+        TWELVE(12)
     }
 
+    data class Coordinates(val fromX: Float, val toX: Float)
+
 }
+
